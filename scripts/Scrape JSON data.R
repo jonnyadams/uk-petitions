0d9a9076c623adcc080800bbd230a11a5a8e5219
Jonny Adams <adams.jonathanpaul@gmail.com>
2018-10-02 21:59:13 +0100

    Now looping through and importing 10,000 petitions

--- scripts/Scrape JSON data.R
+++ scripts/Scrape JSON data.R
@@ -2,12 +2,27 @@ library(jsonlite)
 library (plyr)
 library(tidyverse)
 
-
-url <- "https://petition.parliament.uk/petitions.json?state=open"
+url <- "https://petition.parliament.uk/petitions.json?page=1&state=all"
 
 all_pets_raw <- fromJSON(url)
 
-all_pets <- all_pets_raw$data$attributes
+all_pets_filtered <- all_pets_raw$data$attributes
 
-all_pets_new <- select(all_pets, action:rejection)
+all_pets <- select(all_pets_filtered, action:rejection)
 
+pets_combined <- all_pets
+
+for(i in 2:200){
+  print(i)
+  url <- paste("https://petition.parliament.uk/petitions.json?page=",
+               i, "&state=all", sep="")
+  print(url)
+  all_pets_raw <- fromJSON(url)
+  
+  all_pets_filtered <- all_pets_raw$data$attributes
+  
+  all_pets <- select(all_pets_filtered, action:creator_name)
+  
+  pets_combined <- bind_rows(pets_combined, all_pets)
+  
+}
