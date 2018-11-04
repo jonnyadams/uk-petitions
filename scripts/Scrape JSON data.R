c6a45b8f9de5c38597b6e90081eb5e3be5cded44
Unknown <adams.jonathanpaul@gmail.com>
2018-11-04 16:17:18 +0000

    Major changes- now loop through each petition file to pull detail
    Aggregated up to a single dataset ultimately

--- scripts/Scrape JSON data.R
+++ scripts/Scrape JSON data.R
@@ -11,6 +11,7 @@ all_pets_filtered <- all_pets_raw$data$id
 pets_combined <- all_pets_filtered
 
 for(i in 2:200){
+#for(i in 2:4){
   print(i)
   url <- paste("https://petition.parliament.uk/petitions.json?page=",
                i, "&state=all", sep="")
@@ -23,20 +24,38 @@ for(i in 2:200){
   
 }
 
+counter = 1
 
-url <- "https://petition.parliament.uk/petitions/231147.json"
+for (i in pets_combined) {
+  
+  print(i)
+  url <- paste("https://petition.parliament.uk/petitions/",
+               i, ".json", sep="")
+  print(url)
+  
+  pet_i <- fromJSON(url)
+  
+  pet_i_cons <- pet_i$data$attributes$signatures_by_constituency
+  
+  
+  pet_i_cons <- pet_i$data$attributes$signatures_by_constituency
+  
+  pet_i_cons <- pet_i_cons %>% mutate(id = i)
+  
+  pet_i_details <- pet_i$data$attributes
+  
+  pet_i_cons <- pet_i_cons %>% mutate(action = pet_i_details$action)
+  pet_i_cons <- pet_i_cons %>% mutate(background = pet_i_details$background)
+  pet_i_cons <- pet_i_cons %>% mutate(state = pet_i_details$state)
+  pet_i_cons <- pet_i_cons %>% mutate(created_at = pet_i_details$created_at)
+  pet_i_cons <- pet_i_cons %>% mutate(response_threshold_reached_at = pet_i_details$response_threshold_reached_at)
+  
+  if (counter == 1) { 
+    agg_data <- pet_i_cons
+  } else {
+    agg_data <- bind_rows(agg_data, pet_i_cons)    
+  }
 
-pet_i <- fromJSON(url)
-
-pet_i_cons <- pet_i$data$attributes$signatures_by_constituency
-
-pet_i_cons <- pet_i_cons %>% mutate(id = 231147)
-
-
-state <- pet_i$data$attributes$state
-
-pet_i_cons <- pet_i_cons %>% mutate(state = state)
-
-pet_i_details <- pet_i$data$attributes
-
-pet_i_details <- select(pet_i_details, action:debate)
+  print(counter)
+  counter = counter + 1
+}
