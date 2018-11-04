6f7f0c33c528e05de76b031c52d68f39e379fe4a
Unknown <adams.jonathanpaul@gmail.com>
2018-11-04 14:54:21 +0000

    Attempt to use petition level analysis

--- scripts/Scrape JSON data.R
+++ scripts/Scrape JSON data.R
@@ -6,11 +6,9 @@ url <- "https://petition.parliament.uk/petitions.json?page=1&state=all"
 
 all_pets_raw <- fromJSON(url)
 
-all_pets_filtered <- all_pets_raw$data$attributes
+all_pets_filtered <- all_pets_raw$data$id
 
-all_pets <- select(all_pets_filtered, action:rejection)
-
-pets_combined <- all_pets
+pets_combined <- all_pets_filtered
 
 for(i in 2:200){
   print(i)
@@ -19,10 +17,26 @@ for(i in 2:200){
   print(url)
   all_pets_raw <- fromJSON(url)
   
-  all_pets_filtered <- all_pets_raw$data$attributes
+  all_pets_filtered <- all_pets_raw$data$id
   
-  all_pets <- select(all_pets_filtered, action:creator_name)
-  
-  pets_combined <- bind_rows(pets_combined, all_pets)
+  pets_combined <- combine(pets_combined, all_pets_filtered)
   
 }
+
+
+url <- "https://petition.parliament.uk/petitions/231147.json"
+
+pet_i <- fromJSON(url)
+
+pet_i_cons <- pet_i$data$attributes$signatures_by_constituency
+
+pet_i_cons <- pet_i_cons %>% mutate(id = 231147)
+
+
+state <- pet_i$data$attributes$state
+
+pet_i_cons <- pet_i_cons %>% mutate(state = state)
+
+pet_i_details <- pet_i$data$attributes
+
+pet_i_details <- select(pet_i_details, action:debate)
